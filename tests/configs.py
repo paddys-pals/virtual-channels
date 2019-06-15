57cfbcf849eac3f6fe7e1b502356c6034d78cf6e
Li Xuanji <xuanji@gmail.com>
2019-06-15 16:36:22 -0400

    move contracts to top-level

--- tests/configs.py
+++ tests/configs.py
@@ -1,6 +1,6 @@
 import pathlib
 import os.path
 
-CONTRACT_PATH = pathlib.Path(f"{os.path.dirname(__file__)}/../virtual_channels/contracts")
+CONTRACT_PATH = pathlib.Path(f"{os.path.dirname(__file__)}/../contracts")
 TWO_PARTY_DIRECT_CHANNEL_PATH = CONTRACT_PATH / "TwoPartyDirectChannel.sol"
 SPLITTER_CHANNEL_PATH = CONTRACT_PATH / "Splitter.sol"
